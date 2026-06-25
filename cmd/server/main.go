97202a9ad52ece2c95a205b825ede0b756395417
ordoabchao8 <jimmy.braine44@gmail.com>
2026-06-24 19:33:52 -0500

    changes to main and stuffs

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	const connectionString = "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(connectionString)
+	if err != nil {
+		log.Fatalf("Error creating connection to rabbitMQ. Error:  %s", err)
+	}
+	defer connection.Close()
+	fmt.Println("rabbitMQ connection successful")
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+ 	<-signalChan
+	fmt.Println("\nStop signal recieved. RabbitMQ shutting down.")
+
 }
