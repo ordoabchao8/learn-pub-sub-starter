d3a1605766a7733d0db02cfc6a72da50d128c25d
ordoabchao8 <jimmy.braine44@gmail.com>
2026-07-07 12:27:49 -0500

    Added a way to use gob instead of json to encode data to be sent to a message queue

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,16 +3,17 @@ package main
 import (
 	"fmt"
 	"log"
-	amqp "github.com/rabbitmq/amqp091-go"
-	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
-	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
-	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
-	
+	"time"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
 	fmt.Println("Starting Peril client...")
+	//Create a connection to rabbitMQ
 	const connectionString = "amqp://guest:guest@localhost:5672/"
 	connection, err := amqp.Dial(connectionString)
 	if err != nil {
@@ -20,25 +21,33 @@ func main() {
 	}
 	defer connection.Close()
 	fmt.Println("rabbitMQ connection successful")
+	// Open a channel from the connection 
 	ch, err := connection.Channel()
 	if err != nil {
 		log.Fatalf("Error creating channel from connection. Error: %v", err)
 	}
 	defer ch.Close()
+	// Get a username for player
 	userName, err := gamelogic.ClientWelcome()
 	if err != nil {
 		log.Fatalf("Error creating username. Error: %s", err)
 	}
-	
+	//Create a new game state
 	gameState := gamelogic.NewGameState(userName)
 	err = pubsub.SubscribeJSON(connection, routing.ExchangePerilDirect, routing.PauseKey+"."+userName, routing.PauseKey, pubsub.Transient, handlerPause(gameState))
 	if err != nil {
-		log.Fatalf("Error pausing game. Error: %v", err)
+		log.Fatalf("Error pausing or resuming game. Error: %v", err)
 	}
-	err = pubsub.SubscribeJSON(connection, routing.ExchangePerilTopic, routing.ArmyMovesPrefix+"."+userName, routing.ArmyMovesPrefix+".*", pubsub.Transient, handlerMove(gameState))
+	err = pubsub.SubscribeJSON(connection, routing.ExchangePerilTopic, routing.ArmyMovesPrefix+"."+userName, routing.ArmyMovesPrefix+".*", pubsub.Transient, handlerMove(gameState, ch))
 	if err != nil {
 		log.Fatalf("Error subscribing to move queue. Error: %v", err)
 	}
+
+	err = pubsub.SubscribeJSON(connection, routing.ExchangePerilTopic, routing.WarRecognitionsPrefix, routing.WarRecognitionsPrefix+".*", pubsub.Durable, handlerWar(gameState, ch))
+	if err != nil {
+		log.Fatalf("Error subscribing to war queue. Error: %v", err)
+	}
+
 	for {
 		userInput := gamelogic.GetInput()
 		if len(userInput) == 0 {
@@ -84,16 +93,89 @@ func main() {
 	//fmt.Println("\nStop signal recieved. RabbitMQ shutting down.")
 }
 
-func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) {
-	return func(ps routing.PlayingState) {
+func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) pubsub.Acktype {
+	return func(ps routing.PlayingState) pubsub.Acktype{
 		defer fmt.Print("> ")
 		gs.HandlePause(ps)
+		return pubsub.Ack
 	}
 }
 
-func handlerMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) {
-	return func(am gamelogic.ArmyMove) {
+func handlerMove(gs *gamelogic.GameState, ch *amqp.Channel) func(gamelogic.ArmyMove) pubsub.Acktype {
+	return func(move gamelogic.ArmyMove) pubsub.Acktype {
 		defer fmt.Print("> ")
-		gs.HandleMove(am)
+		moveOutcome := gs.HandleMove(move)
+		switch moveOutcome {
+		case gamelogic.MoveOutComeSafe:
+			return pubsub.Ack
+		case gamelogic.MoveOutcomeMakeWar:
+			err := pubsub.PublishJSON(ch, routing.ExchangePerilTopic, routing.WarRecognitionsPrefix + "." + gs.Player.Username, gamelogic.RecognitionOfWar{
+				Attacker: move.Player,
+				Defender: gs.GetPlayerSnap(),
+			})
+			if err != nil {
+				return pubsub.NackRequeue
+			}
+			return pubsub.Ack
+		case gamelogic.MoveOutcomeSamePlayer:
+			return pubsub.NackDiscard
+		default:
+			return pubsub.NackDiscard
+		}	
 	}
+}
+
+func handlerWar(gs *gamelogic.GameState, ch *amqp.Channel) func(gamelogic.RecognitionOfWar) pubsub.Acktype {
+	return func(war gamelogic.RecognitionOfWar) pubsub.Acktype {
+		defer fmt.Print("> ")
+		outcome, winner, loser := gs.HandleWar(war)
+		switch outcome {
+		case gamelogic.WarOutcomeNotInvolved:
+			return pubsub.NackRequeue
+		case gamelogic.WarOutcomeNoUnits:
+			return pubsub.NackDiscard
+		case gamelogic.WarOutcomeOpponentWon:
+			message := fmt.Sprintf("%s won a war against %s", winner, loser)
+			err := handlerPublishGameLog(gs, ch, message)
+			if err != nil {
+				fmt.Printf("unable to publish gob data in handlerPublishGameLogs. Error: %v", err)
+				return pubsub.NackRequeue
+			}
+			return pubsub.Ack
+		case gamelogic.WarOutcomeYouWon:
+			message := fmt.Sprintf("%s won a war against %s", winner, loser)
+			err := handlerPublishGameLog(gs, ch, message)
+			if err != nil {
+				fmt.Printf("unable to publish gob data in handlerPublishGameLogs. Error: %v", err)
+				return pubsub.NackRequeue
+			}
+			return pubsub.Ack
+		case gamelogic.WarOutcomeDraw:
+			message := fmt.Sprintf("A war between %s and %s resulted in a draw", winner, loser)
+			err := handlerPublishGameLog(gs, ch, message)
+			if err != nil {
+				fmt.Printf("unable to publish gob data in handlerPublishGameLogs. Error: %v", err)
+				return pubsub.NackRequeue
+			}
+			return pubsub.Ack
+		default:
+			fmt.Printf("Unable to declare war, recognition of war failed during outcome processing. %v", outcome)
+			return pubsub.NackDiscard
+		}
+	}
+}
+
+func handlerPublishGameLog(gs *gamelogic.GameState, ch *amqp.Channel, msg string) error {
+	username := gs.GetUsername()
+	exchange := routing.ExchangePerilTopic
+	routing_key := routing.GameLogSlug+"."+username
+	err := pubsub.PublishGob(ch, exchange, routing_key, routing.GameLog{
+		CurrentTime: time.Now(),
+		Message: msg,
+		Username: username,
+	})
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -2,23 +2,37 @@ package pubsub
 
 import (
 	"context"
+	"bytes"
+	"encoding/gob"
 	"encoding/json"
 	"fmt"
+	"log"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+//----------------------------------------------------------------------------------------------------
 type SimpleQueueType int
 
 const (
 	Durable SimpleQueueType = iota
 	Transient
 )
+//----------------------------------------------------------------------------------------------------
+type Acktype int 
 
+const (
+	Ack Acktype = iota
+	NackRequeue
+	NackDiscard
+)
+//----------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------
 func PublishJSON[T any](ch * amqp.Channel, exchange, key string, val T) error {
 	data, err := json.Marshal(val)
 	if err != nil {
-		return err
+		return fmt.Errorf("unable to marshal data to json. Function PublishJSON. Error: %s\n", err)
 	}
 
 	err = ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{
@@ -26,12 +40,12 @@ func PublishJSON[T any](ch * amqp.Channel, exchange, key string, val T) error {
 		Body: data,
 	})
 	if err != nil {
-		return err
+		return fmt.Errorf("unable to publish data to channel. Function PublishJSON. Error: %s\n", err)
 	}
 	return nil
 }
 
-func SubscribeJSON[T any](conn *amqp.Connection, exchange, queueName, key string, queueType SimpleQueueType, handler func(T)) error {
+func SubscribeJSON[T any](conn *amqp.Connection, exchange, queueName, key string, queueType SimpleQueueType, handler func(T) Acktype) error {
 	channel, queue, err := DeclareAndBind(conn, exchange, queueName, key, queueType)
 	if err != nil {
 		return err
@@ -43,15 +57,27 @@ func SubscribeJSON[T any](conn *amqp.Connection, exchange, queueName, key string
 	}
 
 	go func(){
-		for delivery := range deliveryChannel {
+		for msg := range deliveryChannel {
 			var data T
-			err = json.Unmarshal(delivery.Body, &data)
+			err = json.Unmarshal(msg.Body, &data)
 			if err != nil {
 				fmt.Printf("could not unmarshal message: %v\n", err)
 				continue
 			}
-			handler(data)
-			delivery.Ack(false)
+			acktype := handler(data)
+			switch acktype {
+			case Ack:
+				msg.Ack(false)
+				log.Println("Ack")
+			case NackRequeue:
+				msg.Nack(false, true)
+				log.Println("NackRequeue")
+			case NackDiscard:
+				msg.Nack(false, false)
+				log.Println("NackDiscard")
+			default:
+				fmt.Printf("Incorrect acktype: %v. Unable to process message\n", acktype)
+			}	
 		}
 	}()
 	return nil
@@ -60,6 +86,9 @@ func SubscribeJSON[T any](conn *amqp.Connection, exchange, queueName, key string
 func DeclareAndBind(conn *amqp.Connection, exchange, queueName, key string, queueType SimpleQueueType) (*amqp.Channel, amqp.Queue, error) {
 		d := false
 		t := false
+		args := amqp.Table{
+			"x-dead-letter-exchange": "peril_dlx",
+		}
 		if queueType == Durable {
 			d = true
 		}
@@ -71,7 +100,7 @@ func DeclareAndBind(conn *amqp.Connection, exchange, queueName, key string, queu
 			return nil, amqp.Queue{}, err
 		}
 		
-		newQueue, err := channel.QueueDeclare(queueName, d, t, t, false, nil)
+		newQueue, err := channel.QueueDeclare(queueName, d, t, t, false, args)
 		if err != nil {
 			return nil, newQueue, err
 		}
@@ -81,4 +110,21 @@ func DeclareAndBind(conn *amqp.Connection, exchange, queueName, key string, queu
 			return nil, newQueue, err
 		}
 		return channel, newQueue, nil
+}
+
+func PublishGob[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	var buffer bytes.Buffer
+	encoder := gob.NewEncoder(&buffer)
+	err := encoder.Encode(val)
+	if err != nil {
+		return fmt.Errorf("error encoding data to gob. Error: %v", err)
+	}
+	err = ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{
+		ContentType: "application/gob",
+		Body: buffer.Bytes(),
+	})
+	if err != nil {
+		return fmt.Errorf("error publishing data to channel in PublishGob. Error: %v", err)
+	}
+	return nil
 }
